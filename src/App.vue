4fd390a681074b3cc61e356bc4e33d86cff4c167
TheodoroDikuyamaQM <theodoro.dikuyama@quotamedia.co>
2023-12-10 17:09:53 -0500

    fix errorss

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from "vue";
+import {ref} from "vue";
 
 const timePerIcon = 50;
 const indexes = ref([0, 0, 0, 0]);
@@ -18,7 +18,7 @@ const lists = [
     numIcons: 114,
     primaryColor: "#D59C00",
     background_image:
-      "https://firebasestorage.googleapis.com/v0/b/jackpot-game-9c133.appspot.com/o/background-doradobet.webp?alt=media&token=fd0a3a55-5c6b-4052-99f9-82d0cefa2681",
+        "https://firebasestorage.googleapis.com/v0/b/jackpot-game-9c133.appspot.com/o/background-doradobet.webp?alt=media&token=fd0a3a55-5c6b-4052-99f9-82d0cefa2681",
     row_reverse: false,
   },
   {
@@ -28,7 +28,7 @@ const lists = [
     numIcons: 28,
     primaryColor: "#ABC90B",
     background_image:
-      "https://firebasestorage.googleapis.com/v0/b/jackpot-game-9c133.appspot.com/o/background-lotosport.webp?alt=media&token=1e9bdbe1-026e-47f9-9307-9a67076f0e61",
+        "https://firebasestorage.googleapis.com/v0/b/jackpot-game-9c133.appspot.com/o/background-lotosport.webp?alt=media&token=1e9bdbe1-026e-47f9-9307-9a67076f0e61",
     row_reverse: true,
   },
   {
@@ -38,7 +38,7 @@ const lists = [
     numIcons: 121,
     primaryColor: "#FFD028",
     background_image:
-      "https://firebasestorage.googleapis.com/v0/b/jackpot-game-9c133.appspot.com/o/background-ecuabet.webp?alt=media&token=48892d9f-b209-4380-b4ad-42bdb2f9839f",
+        "https://firebasestorage.googleapis.com/v0/b/jackpot-game-9c133.appspot.com/o/background-ecuabet.webp?alt=media&token=48892d9f-b209-4380-b4ad-42bdb2f9839f",
     row_reverse: true,
   },
 ];
@@ -60,22 +60,22 @@ function changeList(listName) {
 
 const roll = (reel, offset = 0) => {
   const iconHeight =
-    window.innerWidth >= 480 ? iconHeightDesktop : iconHeightMobile;
+      window.innerWidth >= 480 ? iconHeightDesktop : iconHeightMobile;
   const delta =
-    (offset + 2) * selectedList.value.numIcons +
-    Math.round(Math.random() * selectedList.value.numIcons); // Number of icons to roll
+      (offset + 2) * selectedList.value.numIcons +
+      Math.round(Math.random() * selectedList.value.numIcons); // Number of icons to roll
   return new Promise((resolve) => {
     const style = getComputedStyle(reel);
     const backgroundPositionY = parseFloat(style["background-position-y"]);
     const targetBackgroundPositionY = backgroundPositionY + delta * iconHeight;
     const normTargetBackgroundPositionY =
-      targetBackgroundPositionY % (selectedList.value.numIcons * iconHeight);
+        targetBackgroundPositionY % (selectedList.value.numIcons * iconHeight);
     setTimeout(() => {
       reel.style.transition = `background-position-y ${
-        (3 + 1 * delta) * timePerIcon
+          (3 + 1 * delta) * timePerIcon
       }ms cubic-bezier(.41,-0.01,.63,1.09)`;
       reel.style.backgroundPositionY = `${
-        backgroundPositionY + delta * iconHeight
+          backgroundPositionY + delta * iconHeight
       }px`;
     }, offset * 150);
 
@@ -91,7 +91,7 @@ const rollAll = () => {
   const reelsList = document.querySelectorAll(".slots .reel");
   Promise.all([...reelsList].map((reel, i) => roll(reel, i))).then((deltas) => {
     deltas.forEach(
-      (delta, i) => (indexes.value[i] = (indexes.value[i] + delta) % numIcons)
+        (delta, i) => (indexes.value[i] = (indexes.value[i] + delta) % numIcons)
     );
     isActive.value = false;
   });
@@ -106,133 +106,85 @@ const toggleActive = () => {
 </script>
 
 <template>
-  <div class="relative w-screen h-screen overflow-hidden">
-    <img
-      :src="selectedList.background_image"
-      alt="background mantenimiento"
-      class="w-full h-full absolute"
-    />
-    <div
-      :class="`absolute mt-8 sm:mt-2 h-[60%] sm:h-screen flex flex-col items-center justify-evenly sm:top-0 top-1/3 ${
+  <div class = "relative w-screen h-screen overflow-hidden">
+    <img :src = "selectedList.background_image" alt = "background mantenimiento" class = "w-full h-full absolute"/>
+    <div :class = "`absolute mt-8 sm:mt-2 h-[60%] sm:h-screen flex flex-col items-center justify-evenly sm:top-0 top-1/3 ${
         selectedList.row_reverse ? 'sm:right-0' : 'sm:left-0'
-      } w-full sm:w-1/2`"
-    >
-      <div class="relative sm:h-[510px] sm:w-[350px] h-[350px] w-[200px]">
-        <img
-          alt="maquina tragamonedas"
-          class="absolute w-full h-full"
-          src="https://firebasestorage.googleapis.com/v0/b/jackpot-game-9c133.appspot.com/o/jackpot.webp?alt=media&token=f1629709-d7ff-48fd-a181-58ae561429a7"
-        />
+      } w-full sm:w-1/2`">
+      <div class = "relative sm:h-[510px] sm:w-[350px] h-[350px] w-[200px]">
+        <img alt = "maquina tragamonedas"
+             class = "absolute w-full h-full"
+             src = "https://firebasestorage.googleapis.com/v0/b/jackpot-game-9c133.appspot.com/o/jackpot.webp?alt=media&token=f1629709-d7ff-48fd-a181-58ae561429a7"/>
         <!--Jackpot-->
-        <div
-          :class="`slots w-[170px] h-[100px] sm:p-[30px] sm:w-[300px] sm:h-[180px] z-10 ${
+        <div :class = "`slots w-[170px] h-[100px] sm:p-[30px] sm:w-[300px] sm:h-[180px] z-10 ${
             isWin1 ? 'win1' : ''
-          } ${isWin2 ? 'win2' : ''}`"
-        >
-          <svg
-            class="absolute top-2 right-[80px] sm:right-36"
-            fill="none"
-            height="8"
-            viewBox="0 0 42 8"
-            width="20%"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <ellipse cx="21" cy="4" fill="white" rx="21" ry="4" />
+          } ${isWin2 ? 'win2' : ''}`">
+          <svg class = "absolute top-2 right-[80px] sm:right-36"
+               fill = "none"
+               height = "8"
+               viewBox = "0 0 42 8"
+               width = "20%"
+               xmlns = "http://www.w3.org/2000/svg">
+            <ellipse cx = "21" cy = "4" fill = "white" rx = "21" ry = "4"/>
           </svg>
-          <svg
-            class="absolute top-2 right-5"
-            fill="none"
-            height="8"
-            viewBox="0 0 97 8"
-            width="30%"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <ellipse cx="48.5" cy="4" fill="white" rx="48.5" ry="4" />
+          <svg class = "absolute top-2 right-5"
+               fill = "none"
+               height = "8"
+               viewBox = "0 0 97 8"
+               width = "30%"
+               xmlns = "http://www.w3.org/2000/svg">
+            <ellipse cx = "48.5" cy = "4" fill = "white" rx = "48.5" ry = "4"/>
           </svg>
-          <div
-            class="flex flex-row w-[140px] h-[80px] sm:w-[320px] sm:h-[130px] rounded-[14px] border-[7px] border-[#746F6F] z-10"
-          >
-            <div class="reel z-10"></div>
+          <div class = "flex flex-row w-[140px] h-[80px] sm:w-[320px] sm:h-[130px] rounded-[14px] border-[7px] border-[#746F6F] z-10">
+            <div class = "reel z-10"></div>
           </div>
-          <svg
-            class="absolute bottom-0 left-0 -z-1"
-            fill="none"
-            height="80%"
-            viewBox="0 0 371 171"
-            width="100%"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M80 12C56.8 -8.40002 17 3.49998 0 12V145C0 165.4 3 170.5 4.5 170.5H371C365.667 159.167 348 135.5 320 131.5C285 126.5 109 37.5 80 12Z"
-              fill="#DE7819"
-              fill-opacity="0.35"
-            />
+          <svg class = "absolute bottom-0 left-0 -z-1"
+               fill = "none"
+               height = "80%"
+               viewBox = "0 0 371 171"
+               width = "100%"
+               xmlns = "http://www.w3.org/2000/svg">
+            <path d = "M80 12C56.8 -8.40002 17 3.49998 0 12V145C0 165.4 3 170.5 4.5 170.5H371C365.667 159.167 348 135.5 320 131.5C285 126.5 109 37.5 80 12Z"
+                  fill = "#DE7819"
+                  fill-opacity = "0.35"/>
           </svg>
-          <svg
-            class="absolute right-1"
-            fill="none"
-            height="90%"
-            viewBox="0 0 8 160"
-            width="5%"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <ellipse
-              cx="4"
-              cy="80"
-              fill="white"
-              rx="80"
-              ry="4"
-              transform="rotate(90 4 80)"
-            />
+          <svg class = "absolute right-1"
+               fill = "none"
+               height = "90%"
+               viewBox = "0 0 8 160"
+               width = "5%"
+               xmlns = "http://www.w3.org/2000/svg">
+            <ellipse cx = "4" cy = "80" fill = "white" rx = "80" ry = "4" transform = "rotate(90 4 80)"/>
           </svg>
         </div>
         <!--Jackpot-->
-        <img
-          alt="palanca"
-          class="absolute top-[175px] -right-3 sm:top-[280px] sm:-right-6 w-[10%] cursor-default"
-          src="https://firebasestorage.googleapis.com/v0/b/jackpot-game-9c133.appspot.com/o/PalancaA.svg?alt=media&token=48dd3d2f-9030-47d1-9252-7f867bb35f76"
-        />
-        <img
-          :class="`${
+        <img alt = "palanca"
+             class = "absolute top-[175px] -right-3 sm:top-[280px] sm:-right-6 w-[10%] cursor-default"
+             src = "https://firebasestorage.googleapis.com/v0/b/jackpot-game-9c133.appspot.com/o/PalancaA.svg?alt=media&token=503631a6-d35a-4f47-b86d-6463c36178c1"/>
+        <img :class = "`${
             isActive ? 'rotate-hor-bottom' : ''
           } cursor-pointer absolute sm:top-[99px] top-[70px] -right-[34px] sm:-right-[61px] w-1/6`"
-          alt="palanca"
-          src="https://firebasestorage.googleapis.com/v0/b/jackpot-game-9c133.appspot.com/o/PalancaB.svg?alt=media&token=cd71a013-e869-4e08-ab07-998f55678e6b"
-          @click="toggleActive"
-        />
+             alt = "palanca"
+             src = "https://firebasestorage.googleapis.com/v0/b/jackpot-game-9c133.appspot.com/o/PalancaB.svg?alt=media&token=f5370023-bae6-4070-9f57-03142285026b"
+             @click = "toggleActive"/>
       </div>
-      <div class="flex justify-around w-1/2">
-        <button
-          class="bg-[#FFD028] text-white font-bold py-2 px-6 rounded-full"
-          @click="changeList('VS')"
-        >
+      <div class = "flex justify-around w-1/2">
+        <button class = "bg-[#FFD028] text-white font-bold py-2 px-6 rounded-full" @click = "changeList('VS')">
           VS
         </button>
-        <button
-          class="bg-[#D59C00] text-white font-bold py-2 px-6 rounded-full"
-          @click="changeList('Quota')"
-        >
+        <button class = "bg-[#D59C00] text-white font-bold py-2 px-6 rounded-full" @click = "changeList('Quota')">
           Quota
         </button>
-        <button
-          class="bg-[#ABC90B] text-white font-bold py-2 px-6 rounded-full"
-          @click="changeList('VS - Quota')"
-        >
+        <button class = "bg-[#ABC90B] text-white font-bold py-2 px-6 rounded-full" @click = "changeList('VS - Quota')">
           VS - Quota
         </button>
       </div>
     </div>
-    <div
-      :class="`absolute flex flex-col items-center mt-6 gap-2 sm:gap-5 sm:justify-center top-0 sm:h-screen  h-1/3  sm:top-0 ${
+    <div :class = "`absolute flex flex-col items-center mt-6 gap-2 sm:gap-5 sm:justify-center top-0 sm:h-screen  h-1/3  sm:top-0 ${
         selectedList.row_reverse ? 'sm:left-0' : 'sm:right-0'
-      } sm:right-0 w-full sm:w-1/2`"
-    >
-      <h1
-        :style="{ color: selectedList.primaryColor }"
-        class="w-[80%] text-center font-extrabold text-7xl"
-      >
-        {{ selectedList.christmas_msg }} {{ selectedList.name }}
-      </h1>
+      } sm:right-0 w-full sm:w-1/2`">
+      <h1 :style = "{ color: selectedList.primaryColor }" class = "w-[80%] text-center font-extrabold text-7xl">
+        {{ selectedList.christmas_msg }} {{ selectedList.name }} </h1>
     </div>
   </div>
 </template>
@@ -285,9 +237,9 @@ const toggleActive = () => {
   height: 100%;
   border-right: 7px solid #746f6f;
   overflow: hidden;
-  background-image: url(../src/assets/images/reel3.png);
+  background-image: url(https://firebasestorage.googleapis.com/v0/b/jackpot-game-9c133.appspot.com/o/reel1.webp?alt=media&token=24ba37e4-bbf4-46d6-a12a-d498340f2225);
   @media screen and (max-width: 480px) {
-    background: url(../src/assets/images/reel1.png);
+    background: url(https://firebasestorage.googleapis.com/v0/b/jackpot-game-9c133.appspot.com/o/reel1.webp?alt=media&token=24ba37e4-bbf4-46d6-a12a-d498340f2225);
   }
   background-position: 0 0;
   background-repeat: repeat-y;
@@ -306,17 +258,16 @@ const toggleActive = () => {
 
 .linear-gradient {
   background: linear-gradient(
-    180deg,
-    #3b405b 0%,
-    rgba(59, 64, 91, 0.72) 30%,
-    rgba(59, 64, 91, 0.38) 62.81%,
-    rgba(59, 64, 91, 0) 100%
+      180deg,
+      #3b405b 0%,
+      rgba(59, 64, 91, 0.72) 30%,
+      rgba(59, 64, 91, 0.38) 62.81%,
+      rgba(59, 64, 91, 0) 100%
   );
 }
 
 .rotate-hor-bottom {
-  -webkit-animation: rotate-hor-bottom 0.8s cubic-bezier(0.645, 0.045, 0.355, 1)
-    both;
+  -webkit-animation: rotate-hor-bottom 0.8s cubic-bezier(0.645, 0.045, 0.355, 1) both;
   animation: rotate-hor-bottom 0.8s cubic-bezier(0.645, 0.045, 0.355, 1) both;
 }
 
